7f49d01f9fa33c0456bd4ff52f4fd6d3be71d2b9
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-01-22 20:25:54 +0000

    add html and css to create quiz area

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var questions = [{
+    question: "What is the capital of Dublin",
+    choices: [2, 5, 10, 15, 20],
+    correctAnswer: 2
+  }, {
+    question: "What is 3*6?",
+    choices: [3, 6, 9, 12, 18],
+    correctAnswer: 4
+  }, {
+    question: "What is 8*9?",
+    choices: [72, 99, 108, 134, 156],
+    correctAnswer: 0
+  }]
