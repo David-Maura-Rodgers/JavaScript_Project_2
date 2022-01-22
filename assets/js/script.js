839e9c73721cf5c45e34a095fe1e46bfbae303ef
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-01-22 20:34:04 +0000

    add questions into array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 var questions = [{
-    question: "What is the capital of Dublin",
-    choices: [2, 5, 10, 15, 20],
+    question: "What is the capital of Ireland",
+    choices: ["Cork", "Sligo", "Dublin", "Galway"],
     correctAnswer: 2
   }, {
-    question: "What is 3*6?",
-    choices: [3, 6, 9, 12, 18],
-    correctAnswer: 4
-  }, {
-    question: "What is 8*9?",
-    choices: [72, 99, 108, 134, 156],
+    question: "Who is the President of America?",
+    choices: ["Joe Biden", "Hillary Clinton", "Barack Obama", "Alex Jones"],
     correctAnswer: 0
+  }, {
+    question: "Jow many sides does an Octagon have?",
+    choices: [4, 12, 19, 8],
+    correctAnswer: 3
   }]
